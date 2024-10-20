4d7fcd276567c7f6bb49d944a4f2e9514e47d493
BBaoVanC <bbaovanc@bbaovanc.com>
2024-10-20 18:19:48 -0500

    WIP wlt-mitm

--- wlt-mitm/src/main.rs
+++ wlt-mitm/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use wayland_client::Connection;
+use snafu::prelude::*;
+
+#[snafu::report]
+fn main() -> Result<(), snafu::Whatever> {
+    let server_conn = Connection::connect_to_env().whatever_context("error connecting to Wayland server")?;
+    Ok(())
 }
